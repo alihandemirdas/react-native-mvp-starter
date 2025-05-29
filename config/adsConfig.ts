3dc5cd498f8cfc1963e1ccb3889cc3ebd4ba4b46
Alihan Demirdas <ben@alihandemirdas.com.tr>
2025-05-29 13:45:01 +0300

    some updates

--- config/adsConfig.ts
+++ config/adsConfig.ts
@@ -10,7 +10,4 @@ export const adUnitIds = {
   rewarded: __DEV__
     ? TestIds.REWARDED
     : "ca-app-pub-9923127969452359/yyyyyyy",
-  appOpen: __DEV__
-    ? TestIds.APP_OPEN
-    : "ca-app-pub-9123127969454459/zzzzzzz",
 };
